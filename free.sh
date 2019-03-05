e237f60be055493ea30cf53924995e616c224242
zac-poe <zachary.poe@rivetlogic.com>
2019-03-05 15:47:16 -0500

    changing to use top instead of vm_stat for better reliability

--- free.sh
+++ free.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 #
-# simple wrapper for vm_stat to resemble linux 'free' but using columns present in Activity Monitor
+# simple wrapper for top to resemble linux 'free' as a Mac port
 #
 
 byte_scale=1024
-block=$(($byte_scale*4))
 
 usage() {
     echo "Usage: $(basename "$0") [-h]"
     echo -e "\nOptions:"
     echo -e "\t-h"
     echo -e "\t\tHuman readable format, byte values are rounded to nearest whole byte (B),"
-    echo -e "\t\tkilobyte (Kb), megabyte (Mb), or gigabyte (Gb)"
+    echo -e "\t\tkilobyte (K), megabyte (M), or gigabyte (G)"
     exit 1
 }
 
 while getopts "h" opt; do
     case "$opt" in
-        h)
-            human_format=1
-        ;;
-        *)
-            usage
-        ;;
+        h) human_format=1;;
+        *) usage;;
     esac
 done
 
@@ -32,48 +27,63 @@ if [[ ${#1} -gt 0 && $(echo "$1" | grep -c '^-') -le 0 ]]; then
 fi
 
 # retrieve actual memory information
-output="$(vm_stat)"
+output="$(top -S -l 1)"
+memory_marker='PhysMem'
+swap_marker='Swap'
 
 get_bytes() {
-    # pages -> bytes
-    echo $(($(echo "$output" | grep "^$1:" | head -n 1 | sed 's/.* \([0-9]*\)\.$/\1/') * $block))
+    stat="$(echo "$output" | grep "^$1:" | sed "s/.* \([0-9MKG]*\) $2.*/\1/")"
+
+    val="${stat:0:$((${#stat} - 1))}"
+    label="${stat:$((${#stat} - 1)):1}"
+
+    while [[ "${#label}" -gt 0 ]]; do
+        val=$(($val * $byte_scale))
+        if [[ "$label" == 'G' ]]; then
+            label='M'
+        elif [[ "$label" == 'M' ]]; then
+            label='K'
+        else
+            label=''
+        fi
+    done
+    echo $val
 }
 
-app_bytes=$(($(get_bytes 'Pages active') + $(get_bytes 'Pages speculative') + $(get_bytes 'Pages purged')))
-free_bytes=$(get_bytes 'Pages free')
-wired_bytes=$(get_bytes 'Pages wired down')
-compressed_bytes=$(get_bytes 'Pages occupied by compressor')
-cache_bytes=$(($(get_bytes 'File-backed pages') + $(get_bytes 'Pages purgeable')))
-used_bytes=$(($app_bytes + $wired_bytes + $compressed_bytes + $cache_bytes))
+used_bytes=$(get_bytes "$memory_marker" 'used')
+free_bytes=$(get_bytes "$memory_marker" 'unused')
+shared_bytes=$(get_bytes 'MemRegions' 'shared')
 total_bytes=$(($used_bytes + $free_bytes))
-swap_bytes=$(get_bytes 'Swapouts')
+swap_used_bytes=$(get_bytes "$swap_marker" '+')
+swap_free_bytes=$(get_bytes "$swap_marker" 'free')
+swap_total_bytes=$(($swap_used_bytes + $swap_free_bytes))
 
 scale_bytes() {
-    val="$(($1 * 100))"
-    label='B'
-    while [[ "$val" -gt "$(($byte_scale * 100))" ]]; do
+    val="$(($1 * 10))"
+    label=''
+
+    while [[ "$val" -gt "$(($byte_scale * 10))" ]]; do
         val=$(($val / $byte_scale))
-        if [[ "$label" == 'B' ]]; then
-            label='Kb'
-        elif [[ "$label" == 'Kb' ]]; then
-            label='Mb'
+        if [[ "${#label}" -le 0 ]]; then
+            label='K'
+        elif [[ "$label" == 'K' ]]; then
+            label='M'
         else
-            label='Gb'
+            label='G'
             break
         fi
     done
-    echo "$(echo "$val" | sed 's/^\(.*\)\([0-9]\{2\}\)$/\1.\2/') $label"
+    echo "$(echo "$val" | sed 's/^\(.*\)\([0-9]\)$/\1.\2/') $label"
 }
 
 if [[ "$human_format" -gt 0 ]]; then
     total_bytes=$(scale_bytes $total_bytes)
     used_bytes=$(scale_bytes $used_bytes)
-    app_bytes=$(scale_bytes $app_bytes)
-    wired_bytes=$(scale_bytes $wired_bytes)
-    compressed_bytes=$(scale_bytes $compressed_bytes)
-    cache_bytes=$(scale_bytes $cache_bytes)
+    shared_bytes=$(scale_bytes $shared_bytes)
     free_bytes=$(scale_bytes $free_bytes)
-    swap_bytes=$(scale_bytes $swap_bytes)
+    swap_used_bytes=$(scale_bytes $swap_used_bytes)
+    swap_free_bytes=$(scale_bytes $swap_free_bytes)
+    swap_total_bytes=$(scale_bytes $swap_total_bytes)
 fi
 
 label_width='5'
@@ -84,11 +94,11 @@ print_formatted() {
         first=''
         field_width="-${field_width}"
     fi
-    printf "%-${label_width}s %${field_width}s %${field_width}s %${field_width}s %${field_width}s %${field_width}s %${field_width}s %${field_width}s\n" \
-        "$first" "$2" "$3" "$4" "$5" "$6" "$7" "$8"
+    printf "%-${label_width}s %${field_width}s %${field_width}s %${field_width}s %${field_width}s\n" \
+        "$first" "$2" "$3" "$4" "$5"
 }
 
 # mimic 'free' style of output
-print_formatted 'HEADING' 'total' 'used' 'free' 'app' 'wired' 'compressed' 'cached'
-print_formatted 'Mem:' "$total_bytes" "$used_bytes" "$free_bytes" "$app_bytes" "$wired_bytes" "$compressed_bytes" "$cache_bytes"
-print_formatted 'Swap:' '' "$swap_bytes"
+print_formatted 'HEADING' 'total' 'used' 'free' 'shared'
+print_formatted 'Mem:' "$total_bytes" "$used_bytes" "$free_bytes" "$shared_bytes"
+print_formatted 'Swap:' "$swap_total_bytes" "$swap_used_bytes" "$swap_free_bytes"
